9527deddb8dc8adee0040be8949304537aa79e44
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-02 16:09:55 +0100

    Added more test cases

--- src/test/java/com/sparta/mua/ProgramTests.java
+++ src/test/java/com/sparta/mua/ProgramTests.java
@@ -3,11 +3,28 @@ package com.sparta.mua;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ProgramTests {
-    @Test
-    @DisplayName("check that 7 return Good Morning")
-    void checkForGoodMorning() {
-        Assertions.assertEquals("Good Morning!", Program.getGreeting(7));
+    @ParameterizedTest
+    @ValueSource(ints = {5,8,11})
+    @DisplayName("check that 5,8,11 return Good Morning!")
+    void checkForGoodMorning(int time) {
+        Assertions.assertEquals("Good Morning!", Program.getGreeting(time));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {12,15,17})
+    @DisplayName("check that 12,15,18 return Good Afternoon!")
+    void checkForGoodAfternoon(int time) {
+        Assertions.assertEquals("Good Afternoon!", Program.getGreeting(time));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,4,18,19,23})
+    @DisplayName("check that 12,15,18 return Good Evening!")
+    void checkForGoodEvening(int time) {
+        Assertions.assertEquals("Good Evening!", Program.getGreeting(time));
     }
 }
