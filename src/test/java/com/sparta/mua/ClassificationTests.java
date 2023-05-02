477c1bf76413b1b745f38516a0e85a5534eaf03b
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-02 18:08:30 +0100

    Added more tests

--- src/test/java/com/sparta/mua/ClassificationTests.java
+++ src/test/java/com/sparta/mua/ClassificationTests.java
@@ -6,31 +6,53 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ClassificationTests {
-    @ParameterizedTest
-    @ValueSource(ints = {1,6,11,12})
-    @DisplayName("check that 1,6,11,12 return U, PG & 12 films are available.")
-    void checkForUnder12(int age) {
-        Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(age));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {13,15})
-    @DisplayName("check that 13,15 return U, PG, 12 & 15 films are available.")
-    void checkForUnder15(int age) {
-        Assertions.assertEquals("U, PG, 12 & 15 films are available.", FilmClassifications.availableClassifications(age));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {16,18,50,127})
-    @DisplayName("check that 16,18,50,127 return All films are available.")
-    void checkForAll(int age) {
-        Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(age));
-    }
 
     @ParameterizedTest
     @ValueSource(ints = {-1,500,128})
     @DisplayName("check that -1,500,128 return Invalid Age!")
     void checkForInvalidAge(int age) {
         Assertions.assertEquals("Invalid Age!", FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,3})
+    @DisplayName("check that 0,1,3 return See guidance for specific film.")
+    void checkForUnder4(int age) {
+        Assertions.assertEquals("See guidance for specific film.", FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4,7})
+    @DisplayName("check that 4,7 return U rated films are available.")
+    void checkForUnder8(int age) {
+        Assertions.assertEquals("U rated films are available.", FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {8,11})
+    @DisplayName("check that 8,11 return U & PG rated films are available.")
+    void checkForUnder12(int age) {
+        Assertions.assertEquals("U & PG rated films are available.", FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {12,14})
+    @DisplayName("check that 12,14 return U, PG & 12 rated films are available.")
+    void checkForUnder15(int age) {
+        Assertions.assertEquals("U, PG & 12 rated films are available.", FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {15,17})
+    @DisplayName("check that 15,17 return U, PG, 12 & 15 rated films are available.")
+    void checkForUnder18(int age) {
+        Assertions.assertEquals("U, PG, 12 & 15 rated films are available.", FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {18,50,100,127})
+    @DisplayName("check that 18,50,100,127 return All films are available.")
+    void checkForOver18(int age) {
+        Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(age));
     }
 }
